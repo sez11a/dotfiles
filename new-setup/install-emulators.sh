2e0719fb28c781f58c1007027c7454ecc7372a5b
Rich Sezov <richard.sezov@liferay.com>
2020-11-10 15:38:41 -0500

    FS-UAE not devel

--- new-setup/install-emulators.sh
+++ new-setup/install-emulators.sh
@@ -4,7 +4,7 @@
 
 sudo pacman -S --noconfirm vice dosemu dosbox
 
-yay -S --noconfirm fs-uae-devel fs-uae-launcher-devel aranym atari800 linapple hfsutils yaze 
+yay -S --noconfirm fs-uae fs-uae-launcher aranym atari800 linapple hfsutils yaze 
 yay -S --noconfirm basiliskii-git         
 mkdir ~/.config/linapple
 cp /usr/share/linapple/config/* ~/.config/linapple
