1e660de0c05aa61611c9252afea4cc5625169277
Rich Sezov <richard.sezov@liferay.com>
2020-11-11 16:37:34 -0500

    Switch to Zulu Java

--- new-setup/install-dev-tools.sh
+++ new-setup/install-dev-tools.sh
@@ -3,7 +3,7 @@
 yay -S --noconfirm zulu-11-bin zulu-8-bin squirrel-sql 
 sudo archlinux-java set zulu-11
 
-sudo pacman -S --noconfirm source-highlight qgit lazarus-qt5 fpc gdb nodejs npm kdiff3 netbeans
+sudo pacman -S --noconfirm source-highlight qgit lazarus-qt5 fpc gdb nodejs npm kdiff3 netbeans github-cli
 
 mkdir ~/.npm-packages
 NPM_PACKAGES=~/.npm-packages
