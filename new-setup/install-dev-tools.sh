5fdd20ca3e6e149a0351c52f18f370fbaa42ccd3
Rich Sezov <richard.sezov@liferay.com>
2023-01-31 11:56:05 -0500

    Add GitUI

--- new-setup/install-dev-tools.sh
+++ new-setup/install-dev-tools.sh
@@ -3,7 +3,7 @@
 yay -S --noconfirm zulu-11-bin zulu-8-bin squirrel-sql
 sudo archlinux-java set zulu-11
 
-sudo pacman -S --noconfirm source-highlight lazarus-qt5 fpc gdb nodejs npm kdiff3 netbeans github-cli ruby rubygems
+sudo pacman -S --noconfirm source-highlight lazarus-qt5 fpc gdb nodejs npm kdiff3 netbeans github-cli ruby rubygems gitui
 
 yay -S --noconfirm rbenv gitahead
 
