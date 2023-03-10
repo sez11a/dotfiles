6c7aa4162c5172c9910d49a2a109f6f3267ee757
Rich Sezov <richard.sezov@liferay.com>
2023-03-10 17:52:32 -0500

    Fixed emulators

--- new-setup/install-emulators.sh
+++ new-setup/install-emulators.sh
@@ -4,7 +4,8 @@
 
 sudo pacman -S --noconfirm vice dosemu dosbox
 
-yay -S --noconfirm fs-uae fs-uae-launcher aranym atari800 linapple hfsutils yaze 
+yay -S --noconfirm fs-uae fs-uae-launcher aranym atari800 hfsutils yaze 
 yay -S --noconfirm basiliskii-git         
+yay -S --noconfirm linapple-git
 mkdir ~/.config/linapple
 cp /usr/share/linapple/config/* ~/.config/linapple
