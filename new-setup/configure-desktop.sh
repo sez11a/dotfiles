6bd1bd89a433a31fa18d39acd23d676ea22ba2d4
Rich Sezov <richard.sezov@liferay.com>
2024-02-08 11:01:19 -0500

    Add Docker

--- new-setup/configure-desktop.sh
+++ new-setup/configure-desktop.sh
@@ -175,7 +175,7 @@ if echo "$DesktopApps" | grep -iq "^y" ;then
     # Standard desktop stuff
 
     sudo pacman -R --noconfirm libreoffice-still
-    sudo pacman -S --noconfirm xsel libdvdcss bottom pandoc-cli bash-completion audacity borg calibre neovide mc tlp p7zip whois projectm easytag fuse handbrake tk scribus vpnc networkmanager-vpnc fontforge kdiff3 dvgrab dvdauthor inkscape strawberry conky libreoffice-fresh offlineimap dovecot neomutt w3m urlscan lha zip unzip vifm xdg-desktop-portal filelight mplayer fzf ripgrep the_silver_searcher fd ranger libaacs mpv smplayer smplayer-skins smplayer-themes ctags pstoedit libmythes beanshell coin-or-mp yt-dlp atomicparsley aria2 discord cdrdao cdrtools libisofs dvd+rw-tools hunspell hunspell-en_us hyphen hyphen-en
+    sudo pacman -S --noconfirm xsel libdvdcss bottom pandoc-cli bash-completion audacity borg python-llfuse calibre neovide mc tlp p7zip whois projectm easytag fuse handbrake tk scribus vpnc networkmanager-vpnc fontforge kdiff3 dvgrab dvdauthor inkscape strawberry conky libreoffice-fresh offlineimap dovecot neomutt w3m urlscan lha zip unzip vifm xdg-desktop-portal filelight mplayer fzf ripgrep the_silver_searcher fd ranger libaacs mpv smplayer smplayer-skins smplayer-themes ctags pstoedit libmythes beanshell coin-or-mp yt-dlp atomicparsley aria2 discord cdrdao cdrtools libisofs dvd+rw-tools hunspell hunspell-en_us hyphen hyphen-en
 
     # Apps in AUR
 
--- new-setup/install-dev-tools.sh
+++ new-setup/install-dev-tools.sh
@@ -4,6 +4,8 @@ yay -S --noconfirm squirrel-sql
 
 sudo pacman -S --noconfirm source-highlight lazarus-qt5 fpc gdb nodejs npm kdiff3 netbeans github-cli ruby rubygems gitui
 
+sudo pacman -S --noconfirm docker docker-compose 
+
 yay -S --noconfirm rbenv gitahead
 
 mkdir ~/.npm-packages
