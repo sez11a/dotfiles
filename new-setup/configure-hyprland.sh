94b625410fe1def9217cd49846d790228cce5fab
Rich Sezov <richard.sezov@liferay.com>
2024-07-10 15:53:20 -0400

    Delete hyprland for now

--- new-setup/configure-hyprland.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo pacman -S --noconfirm hyprland xdg-desktop-portal-hyprland kitty grim slurp swappy otf-font-awesome
-sudo pacman -S --noconfirm swaync hypridle hyprlock hyprpaper hyprwayland-scanner waybar wofi cliphist udiskie
-sudo pacman -S --noconfirm lxappearance qt6ct qt5ct nwg-look
