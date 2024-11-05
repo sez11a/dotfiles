4ddd4a5f91edad6995ea7852e45b08b2be25856d
Rich Sezov <richard.sezov@liferay.com>
2024-11-05 16:55:58 -0500

    Remove check from plasma; superslicer no longer in repo.

--- new-setup/arch/6-3d-printing.sh
+++ new-setup/arch/6-3d-printing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm superslicer prusa-slicer freecad openscad platformio-core platformio-core-udev
+sudo pacman -S --noconfirm prusa-slicer freecad openscad platformio-core platformio-core-udev
 yay -S --noconfirm cura-bin
 yay -S --noconfirm orca-slicer-bin
 yay -S --noconfirm ondsel-es-appimage
