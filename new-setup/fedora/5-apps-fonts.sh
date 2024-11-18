aa77762dd93e57eddd37ea17663e910cc3586832
Rich Sezov <richard.sezov@liferay.com>
2024-11-18 14:56:00 -0500

    Add worker to Fedora

--- new-setup/fedora/5-apps-fonts.sh
+++ new-setup/fedora/5-apps-fonts.sh
@@ -20,7 +20,7 @@ sudo dnf -qy install https://cdn.azul.com/zulu/bin/zulu-repo-1.0.0-1.noarch.rpm
 sudo dnf -qy install zulu21-jdk
 
 # Standard desktop stuff
-sudo dnf -qy install pandoc audacity borgbackup spamassassin perl-Razor-Agent calibre neovim-qt mc p7zip easytag HandBrake HandBrake-gui scribus fontforge kdiff3 dvgrab dvdauthor inkscape strawberry conky lha vifm mplayer fzf ripgrep ranger libaacs mpv smplayer smplayer-themes pstoedit yt-dlp AtomicParsley aria2 cdrdao libisofs dvd+rw-tools libbdplus joe 
+sudo dnf -qy install pandoc audacity borgbackup spamassassin perl-Razor-Agent calibre neovim-qt mc p7zip easytag HandBrake HandBrake-gui scribus fontforge kdiff3 dvgrab dvdauthor inkscape strawberry conky lha vifm mplayer fzf ripgrep ranger libaacs mpv smplayer smplayer-themes pstoedit yt-dlp AtomicParsley aria2 cdrdao libisofs dvd+rw-tools libbdplus joe worker
 sudo dnf -qy copr enable mdwalters/neovide
 sudo dnf -qy install neovide
 
