2c39f7f53420d0046fa9b5ac5aabf2c17ac6267e
Rich Sezov <richard.sezov@liferay.com>
2025-04-15 12:35:15 -0400

    Try to fix fonts.

--- new-setup/OM/3-os.sh
+++ new-setup/OM/3-os.sh
@@ -17,19 +17,29 @@ sudo dnf -y install tlp
 
 # UI Fonts
 sudo dnf -y install texlive-libertinus texlive-libertinus-fonts texlive-libertinus-otf texlive-libertinus-type1 texlive-libertinust1math
+curl -OL https://github.com/alerque/libertinus/releases/download/v7.051/Libertinus-7.051.tar.zst
+mkdir ~/Downloads/Libertinus 
+tar xvfz Libertinus-7.051.tar.zst -C ~/Downloads/Libertinus
+rm Libertinus-7.051.tar.zst
+
 curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Iosevka.tar.xz
 mkdir ~/Downloads/Iosevka
 tar xvfz Iosevka.tar.xz -C ~/Downloads/Iosevka
+rm Iosevka.tar.xz
 curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/IosevkaTerm.tar.xz
 mkdir ~/Downloads/IosevkaTerm
 tar xvfz IosevkaTerm.tar.xz -C ~/Downloads/IosevkaTerm
+rm IosevkaTerm.tar.xz
 curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/IosevkaTermSlab.tar.xz
 mkdir ~/Downloads/IosevkaTermSlab
 tar xvfz IosevkaTermSlab.tar.xz -C ~/Downloads/IosevkaTermSlab
+rm IosevkaTermSlab.tar.xz
 
-sudo cp ~/Downloads/Iosevka/*.ttf /usr/share/fonts/TTF 
-sudo cp ~/Downloads/IosevkaTerm/*.ttf /usr/share/fonts/TTF 
-sudo cp ~/Downloads/IosevkaTermSlab/*.ttf /usr/share/fonts/TTF 
+sudo mkdir /usr/share/fonts/custom
+sudo cp ~/Downloads/Libertinus/Libertinus-7.051/static/OTF/* /usr/share/fonts/custom
+sudo cp ~/Downloads/Iosevka/*.ttf /usr/share/fonts/custom 
+sudo cp ~/Downloads/IosevkaTerm/*.ttf /usr/share/fonts/custom 
+sudo cp ~/Downloads/IosevkaTermSlab/*.ttf /usr/share/fonts/custom 
 sudo fc-cache -f -v
 
 sudo cp conf/fonts-local.conf /etc/fonts/local.conf
--- new-setup/common/fonts.sh
+++ new-setup/common/fonts.sh
@@ -1,9 +1,8 @@
 #!/bin/bash 
 
 cp conf/fonts.conf ~/.config/fontconfig
-sudo mkdir /usr/share/fonts/TTF
-sudo mkdir /usr/share/fonts/OTF
-sudo cp fonts/*.ttf /usr/share/fonts/TTF
-sudo cp fonts/*.otf /usr/share/fonts/OTF
+sudo mkdir /usr/share/fonts/custom
+sudo cp fonts/*.ttf /usr/share/fonts/custom
+sudo cp fonts/*.otf /usr/share/fonts/custom
 sudo fc-cache -f -v
 
