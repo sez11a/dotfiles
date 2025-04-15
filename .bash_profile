1d4f63b49155670d140605d1d6e6ee162ca19f84
Rich Sezov <richard.sezov@liferay.com>
2025-04-15 14:50:03 -0400

    Move things around; add dsync alias

--- .bash_profile
+++ .bash_profile
@@ -4,4 +4,6 @@
 
 [[ -f ~/.extend.bash_profile ]] && . ~/.extend.bash_profile
 [[ -f ~/.bashrc ]] && . ~/.bashrc
+PATH=$PATH:$HOME/.local/bin
+export PATH
 
--- new-setup/OM/1-preliminary.sh
+++ new-setup/OM/1-preliminary.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+## Starship 
+curl -sS https://starship.rs/install.sh | sh
+
 sudo enable-repo extra 
 sudo enable-repo restricted
 sudo enable-repo non-free
+
+# Configure DNF 
+echo -e "${NOTE} Adding extra spice in dnf.conf ... ${RESET}" 2>&1
+echo "max_parallel_downloads=10" | sudo tee -a /etc/dnf/dnf.conf
+echo -e "${CAT} dnf.conf spicing up completed ${RESET}" 2>&1
+
 sudo dnf clean all ; dnf clean all ; sudo dnf distro-sync --refresh --allowerasing 2>&1| tee dsync2-log.txt
 sudo dnf --refresh -y install newt stow
 sudo dnf -y install task-devel task-c-devel task-c++-devel clang llvm
@@ -10,11 +19,8 @@ sudo dnf -y install task-devel task-c-devel task-c++-devel clang llvm
 DIALOG=whiptail
 
 # Dotfiles
-
 source common/dotfiles.sh
-
-## Starship 
-curl -sS https://starship.rs/install.sh | sh
+echo 'alias dsync="sudo dnf clean all ; dnf clean all ; sudo dnf distro-sync --refresh --allowerasing 2>&1| tee dsync2-log.txt ; flatpak update" # dnf updates for ROME' | tee -a ~/.profile
 
 # Set up for installation
 sudo dnf -y install festival 
@@ -28,11 +34,6 @@ sudo dnf -y install flatpak
 sudo wget https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo -O /etc/yum.repos.d/brave-browser.repo
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo dnf -y install brave-browser
-
-# Configure DNF 
-echo -e "${NOTE} Adding extra spice in dnf.conf ... ${RESET}" 2>&1
-echo "max_parallel_downloads=10" | sudo tee -a /etc/dnf/dnf.conf
-echo -e "${CAT} dnf.conf spicing up completed ${RESET}" 2>&1
 
 # Install dnfdragora
 sudo dnf -y install dnfdragora
