50b28d2f41794231466b93207872a27b4a8eb38a
Rich Sezov <richard.sezov@liferay.com>
2025-04-16 17:15:57 -0400

    Add nextcloud-client

--- new-setup/OM/7-dev-tools.sh
+++ new-setup/OM/7-dev-tools.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 
-sudo dnf -y install lazarus nodejs lazygit yarnpkg podman podman-compose
+sudo dnf -y install lazarus nodejs lazygit podman podman-compose
 sudo dnf -y install 'dnf-command(config-manager)'
 sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
 sudo wget https://cli.github.com/packages/rpm/gh-cli.repo -O /etc/yum.repos.d/gh-cli.repo
