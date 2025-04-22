a748432774fa1302deff39b8d5dcad9d8c57b699
Rich Sezov <richard.sezov@liferay.com>
2025-04-21 23:06:56 -0400

    Add keepassxc

--- new-setup/OM/5-apps-fonts.sh
+++ new-setup/OM/5-apps-fonts.sh
@@ -11,7 +11,7 @@ sudo dnf -y install https://cdn.azul.com/zulu/bin/zulu-repo-1.0.0-1.noarch.rpm
 sudo dnf -y install zulu21-jdk
 
 # Standard desktop stuff
-sudo dnf -y install audacity borgbackup spamassassin calibre mc easytag handbrake scribus fontforge kdiff3 dvgrab dvdauthor inkscape strawberry lha mplayer fzf ranger mpv smplayer smplayer-themes pstoedit yt-dlp atomicparsley aria2 cdrdao dvd+rw-tools joe lib64aacs0 lib64aacs-devel nextcloud-client hunspell hunspell-tools hyphen-en 
+sudo dnf -y install audacity borgbackup spamassassin calibre mc easytag handbrake scribus fontforge kdiff3 dvgrab dvdauthor inkscape keepassxc strawberry lha mplayer fzf ranger mpv smplayer smplayer-themes pstoedit yt-dlp atomicparsley aria2 cdrdao dvd+rw-tools joe lib64aacs0 lib64aacs-devel nextcloud-client hunspell hunspell-tools hyphen-en 
 
 # Standard Flatpak stuff
 sudo flatpak -y install zoom
