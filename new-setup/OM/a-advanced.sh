8648a2ce801636dc1b161cef2cd15f9af5c2ad1e
Rich Sezov <richard.sezov@liferay.com>
2025-08-21 23:44:12 -0400

    Add zram config to advanced.

--- new-setup/OM/a-advanced.sh
+++ new-setup/OM/a-advanced.sh
@@ -11,3 +11,12 @@ sudo dnf -y install offlineimap dovecot neomutt w3m urlscan discord
 ## Undelete files
 sudo dnf -y install ddrescue extundelete
 
+## Zram
+sudo dnf install zram-init
+sudo modprobe zram
+sudo systemctl start zram_swap.service
+sudo systemctl start zram_tmp.service
+sudo systemctl start zram_var_tmp.service
+sudo systemctl enable zram_swap.service
+sudo systemctl enable zram_tmp.service
+sudo systemctl enable zram_var_tmp.service
