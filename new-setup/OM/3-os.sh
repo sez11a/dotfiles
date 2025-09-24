0fe7abab73711b2c115ba30642211c8280225b25
Rich Sezov <richard.sezov@liferay.com>
2025-09-24 15:19:16 -0400

    Starting to re-tool for OM's UM.

--- new-setup/OM/3-os.sh
+++ new-setup/OM/3-os.sh
@@ -25,25 +25,29 @@ curl -OL https://github.com/alerque/libertinus/releases/download/v7.051/Libertin
 mkdir ~/Downloads/Libertinus
 tar xvfz Libertinus-7.051.tar.zst -C ~/Downloads/Libertinus
 rm Libertinus-7.051.tar.zst
-
-curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Iosevka.tar.xz
-mkdir ~/Downloads/Iosevka
-tar xvfz Iosevka.tar.xz -C ~/Downloads/Iosevka
-rm Iosevka.tar.xz
-curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/IosevkaTerm.tar.xz
-mkdir ~/Downloads/IosevkaTerm
-tar xvfz IosevkaTerm.tar.xz -C ~/Downloads/IosevkaTerm
-rm IosevkaTerm.tar.xz
-curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/IosevkaTermSlab.tar.xz
-mkdir ~/Downloads/IosevkaTermSlab
-tar xvfz IosevkaTermSlab.tar.xz -C ~/Downloads/IosevkaTermSlab
-rm IosevkaTermSlab.tar.xz
-
 sudo mkdir /usr/share/fonts/custom
 sudo cp ~/Downloads/Libertinus/Libertinus-7.051/static/OTF/* /usr/share/fonts/custom
-sudo cp ~/Downloads/Iosevka/*.ttf /usr/share/fonts/custom
-sudo cp ~/Downloads/IosevkaTerm/*.ttf /usr/share/fonts/custom
-sudo cp ~/Downloads/IosevkaTermSlab/*.ttf /usr/share/fonts/custom
+
+sudo dnf -y install fonts-ttf-iosevka fonts-ttf-iosevkaterm fonts-ttf-iosevkatermslab
+
+# Necessary for Rock 6.0
+# curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Iosevka.tar.xz
+# mkdir ~/Downloads/Iosevka
+# tar xvfz Iosevka.tar.xz -C ~/Downloads/Iosevka
+# rm Iosevka.tar.xz
+# curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/IosevkaTerm.tar.xz
+# mkdir ~/Downloads/IosevkaTerm
+# tar xvfz IosevkaTerm.tar.xz -C ~/Downloads/IosevkaTerm
+# rm IosevkaTerm.tar.xz
+# curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/IosevkaTermSlab.tar.xz
+# mkdir ~/Downloads/IosevkaTermSlab
+# tar xvfz IosevkaTermSlab.tar.xz -C ~/Downloads/IosevkaTermSlab
+# rm IosevkaTermSlab.tar.xz
+
+# sudo cp ~/Downloads/Iosevka/*.ttf /usr/share/fonts/custom
+# sudo cp ~/Downloads/IosevkaTerm/*.ttf /usr/share/fonts/custom
+# sudo cp ~/Downloads/IosevkaTermSlab/*.ttf /usr/share/fonts/custom
+
 sudo fc-cache -f -v
 
 sudo cp conf/fonts-local.conf /etc/fonts/local.conf
