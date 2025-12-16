49106bebd5de3c237acc1513109f8ab21313f37f
Rich Sezov <richard.sezov@liferay.com>
2025-12-16 11:54:12 -0500

    Fix prompt, switch back to Docker, fix update command for DNF5.

--- new-setup/OM/7-dev-tools.sh
+++ new-setup/OM/7-dev-tools.sh
@@ -1,27 +1,44 @@
 #!/bin/sh
 
+## Git and GitHub CLI
+sudo dnf -y install 'dnf-command(config-manager)'
+sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+sudo wget https://cli.github.com/packages/rpm/gh-cli.repo -O /etc/yum.repos.d/gh-cli.repo
+sudo dnf -y install gh --repo gh-cli
+sudo dnf -y install lazygit colordiff
+
+# Packaging
 sudo dnf -y install task-devel task-c-devel task-c++-devel clang llvm lrzip rpm-build packaging-tools
+
+# Python
 sudo dnf -y install python-ensurepip python-virtualenv
-sudo dnf -y install lazarus nodejs lazygit
+
+# Pascal
+sudo dnf -y install lazarus
 
 ## Java Environment
 sudo dnf -y install https://cdn.azul.com/zulu/bin/zulu-repo-1.0.0-1.noarch.rpm
 sudo dnf -y install zulu21-jdk
 sudo alternatives --install /usr/lib/jvm/jdk jdk /usr/lib/jvm/java-21-zulu-openjdk-ca 1
-
-sudo dnf -y install podman podman-compose crun passt colordiff
 sudo dnf -y install ant gradle
+
+# Virtual Machines
+sudo dnf install virt-manager qemu-img iptables
+
+# Containers
+sudo dnf -y install docker docker-cli docker-compose lazydocker
+sudo systemctl enable docker
+sudo usermod -a -G docker $USER
+# sudo dnf -y install podman podman-compose crun passt
+
+# Ruby
 sudo dnf -y install ruby
 sudo dnf -y install rbenv
 
-## GitHub CLI
-sudo dnf -y install 'dnf-command(config-manager)'
-sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
-sudo wget https://cli.github.com/packages/rpm/gh-cli.repo -O /etc/yum.repos.d/gh-cli.repo
-sudo dnf -y install gh --repo gh-cli
-
+# Node
+sudo dnf -y install nodejs
 mkdir ~/.npm-packages
 NPM_PACKAGES=~/.npm-packages
 echo prefix=~/.npm-packages >> ~/.npmrc
 
-flatpak -y install com.github.Murmele.Gittyup squirrel_sql PodmanDesktop
+flatpak -y install com.github.Murmele.Gittyup squirrel_sql
--- new-setup/OM/update.sh
+++ new-setup/OM/update.sh
@@ -4,7 +4,7 @@ echo -e "\033[0;31m╔═══════════════════�
 echo -e "\033[0;31m║**Updating all System packages**║\033[0m" # Red for root.
 echo -e "\033[0;31m╚════════════════════════════════╝\033[0m" # Red for root.
 echo
-sudo dnf clean all ; dnf clean all ; sudo dnf distro-sync --refresh --allowerasing 2>&1| tee dsync2-log.txt
+sudo dnf clean all ; sudo dnf dsync --allowerasing 2>&1| tee dsync2-log.txt
 echo
 echo -e "\033[0;33m╔══════════════════════════════════╗\033[0m" # Yellow for user
 echo -e "\033[0;33m║**Updating Flatpaks from FlatHub**║\033[0m" # Yellow for user
