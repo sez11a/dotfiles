e9c69dc9c12c1b07b1c791fcb6b389182f3882b7
Rich Sezov <sezovr@gmail.com>
2026-07-15 16:25:27 -0400

    Package not used and not available in Debian.

--- new-setup/debian/6-3d-printing.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Installing 3D Printing apps...."
-
-snap install orcaslicer cura-slicer freecad
-sudo apt install -y openscad
-
-curl -fsSL -o get-platformio.py https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py
-python3 get-platformio.py
--- new-setup/debian/9-emulators.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sudo apt install -y fs-uae fs-uae-arcade atari800 aranym hatari vice dosbox-x basilisk2 hfsutils
